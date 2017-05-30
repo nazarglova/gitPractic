5c781054886e5eec64acab27b78d4f66ee5272bf
Nazariy <nazarglova@gmail.com>
2017-05-31 00:06:03 +0300

    not work custom Apply

---
+++ bind.js
@@ -0,0 +1,17 @@
+'use strict'
+
+var a = {
+    namer: 'mihal',
+    lastName: 'petrov'
+}
+function print() {
+    console.log(this.namer + ' ' + this.lastName);
+}
+
+function nApply(self, arr) {
+    let nawSelf =  Object.assign({}, self);
+    nawSelf[this.name] = this;
+    return nawSelf[this.name](...arr);
+}
+print.nApply(a, 5);
+print.call(a)
